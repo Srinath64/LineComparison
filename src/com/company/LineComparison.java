68d2e031ce844085e36ba911042db517a8c406e9
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-13 19:51:26 +0530

    First commit

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -1,19 +1,7 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class LineComparison {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Co-ordinates:");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        System.out.println("Enter Second Co-ordinates:");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
-        double length;
-
-        length = Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
-        System.out.println("The length of line is:" +length);
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch ");
     }
 }
