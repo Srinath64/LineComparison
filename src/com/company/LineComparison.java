181df6a967215f5ef6b5daecf92b3f3627c8f5f5
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-13 19:53:54 +0530

    Added UC2

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -1,7 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program on Master Branch ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Co-ordinates:");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Enter Second Co-ordinates:");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        System.out.println("Enter Third Co-ordinates:");
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+        System.out.println("Enter Fourth Co-ordinates:");
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+        double length1,length2;
+
+        length1 = Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
+        System.out.println("The length of First line is:" +length1);
+        length2 = Math.sqrt(Math.pow((x4-x3),2)+ Math.pow((y4-y3),2));
+        System.out.println("The length of Second line is:" +length2);
+
+        if(length1 == length2) {
+            System.out.println("Lines Are Equals");
+        }else {
+            System.out.println("Lines Are Not Equals");
+        }
     }
 }
