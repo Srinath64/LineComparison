aebf699dc81494a4f333de891677cbac899c19e2
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-13 19:47:19 +0530

    Added UC1

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -1,8 +1,19 @@
 package com.company;
 
-public class LineComparison {
+import java.util.Scanner;
 
+public class LineComparison {
     public static void main(String[] args) {
-        System.out.println(" Welcome to Line Comparison Computation Program ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Co-ordinates:");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Enter Second Co-ordinates:");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        double length;
+
+        length = Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
+        System.out.println("The length of line is:" +length);
     }
 }
